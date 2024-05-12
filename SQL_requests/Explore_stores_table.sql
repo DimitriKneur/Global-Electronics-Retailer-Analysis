4b5481e14a62d3281a5e3647a1c0338a41734772
DimitriKneur <dimitri.kneur@gmail.com>
2024-05-12 11:09:01 +0200

    starting EDA

--- SQL_requests/Explore_stores_table.sql
+++ SQL_requests/Explore_stores_table.sql
@@ -11,3 +11,17 @@ VALUES
 
 -- Snapshot of the stores table after addition of the online store--
 SELECT * FROM stores;
+
+-- Number of stores by country --
+SELECT COUNT(DISTINCT `StoreKey`) AS NumberOfStores, `Country`
+FROM stores
+GROUP BY `Country`
+ORDER BY NumberOfStores DESC
+LIMIT 5;
+
+-- Number of stores by state in the US --
+SELECT COUNT(DISTINCT `StoreKey`) AS NumberOfStores, `State`
+FROM stores
+WHERE `Country` = 'United States'
+GROUP BY `State`
+ORDER BY NumberOfStores DESC;
--- SQL_requests/Join_tables.sql
+++ SQL_requests/Join_tables.sql
@@ -1,1 +1,29 @@
-SELECT * FROM 
+SELECT stores.`StoreKey`,
+	   stores.`State` as StoreState,
+       stores.`Country` as StoreCountry,
+       stores.`Open Date` as StoreOpenDate,
+       stores.`Square Meters` as StoreSurfaceAsSquareMeters,
+       sales.`Order Number`,
+	   sales.`Order Date`, 
+	   products.`ProductKey`,
+       products.`Product Name`,
+       products.`Brand`,
+       products.`Category`,
+       products.`Subcategory`,
+       products.`Unit Cost USD`,
+       products.`Unit Price USD`,
+       sales.`Currency Code`,
+       rates.`Exchange` AS ExchangeVsUSD,
+	   sales.`Quantity`,
+       customers.`CustomerKey`,
+       customers.`Birthday`,
+       customers.`Gender`,
+       customers.`City` AS CustomerCity,
+       customers.`State` AS CustomerState,
+       customers.`Country` AS CustomerCountry,
+       customers.`Continent` AS CustomerContinent
+       FROM sales
+JOIN exchange_rates AS rates ON sales.`Order Date` = rates.`Date` AND sales.`Currency Code` = rates.`Currency`
+JOIN products ON sales.`ProductKey` = products.`ProductKey`
+JOIN customers ON sales.`CustomerKey` = customers.`CustomerKey`
+JOIN stores ON sales.`StoreKey` = stores.`StoreKey`;
